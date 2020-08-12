8111f9425e8995ca233eea31a56f4b78b7dbd989
Shubham Saurav <shubhamsinha186@gmail.com>
2020-08-12 22:02:45 +0530

    Relationships Created

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,8 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Customer, Product, Order
+from .models import Customer, Product, Order, Tag
+# Can also do this
+# from .models import *
+
+
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Tag)
--- accounts/models.py
+++ accounts/models.py
@@ -17,6 +17,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=128, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -25,8 +32,12 @@ class Product(models.Model):
     name = models.CharField(max_length=128, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=128, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=600, null=True)
+    description = models.CharField(max_length=600, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -35,5 +46,8 @@ class Order(models.Model):
         ('Out for Delivery', 'Out for Delivery'),
         ('Delivered', 'Delivered')
     )
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=32, null=True, choices=STATUS)
     date_created = models.DateTimeField(auto_now_add=True, null=True)

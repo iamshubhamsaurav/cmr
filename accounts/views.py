fc4b63ec9f67064a2254def58ce472ccdf0c9636
Shubham Saurav <shubhamsinha186@gmail.com>
2020-08-14 20:46:00 +0530

    Rendering data in dashboard and products

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,32 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+# from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import *
 
 
 def home(request):
     # return HttpResponse('Home')
-    return render(request, 'accounts/dashboard.htm')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+
+    pending = orders.filter(status="Pending").count()
+    delivered = orders.filter(status="Delivered").count()
+
+    context = {'orders': orders, 'customers': customers, 'total_customers': total_customers,
+               'total_orders': total_orders, 'pending': pending, 'delivered': delivered}
+    return render(request, 'accounts/dashboard.htm', context)
 
 
 def customers(request):
-    return render(request, 'accounts/customers.htm')
+    customers = Customer.objects.all()
+    return render(request, 'accounts/customers.htm', {'customers': customers})
 
 
 def products(request):
-    return render(request, 'accounts/products.htm')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.htm', {'products': products})
